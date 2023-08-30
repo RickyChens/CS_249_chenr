beb29d72453457f3d40aa431587133b92500d225
Ricky Chen <chenr@sunypoly.edu>
2023-08-30 12:29:13 -0400

    Testing Branch

--- howdy/src/main/java/edu/realemj/howdy/HowdyPrinter.java
+++ howdy/src/main/java/edu/realemj/howdy/HowdyPrinter.java
@@ -2,14 +2,7 @@ package edu.realemj.howdy;
 
 public class HowdyPrinter {
     public static void main(String [] args) {
-        System.out.println("*********************");
-        System.out.println("HELLO!  It's me.");
-        System.out.println("How are you?");
-        System.out.println("I'm fine!");
-        System.out.println("So long!");
-        System.out.println("Farewell!");
-        System.out.println("Goodbye!");
-        System.out.println("*********************");
+        System.out.println("HELLO!");
     }
 
     public static String getPassword() {
